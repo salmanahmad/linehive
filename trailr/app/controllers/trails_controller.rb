81426f8d50b18de6080fa1968f24021cc3da1271
Salman Ahmad <salmanahmad@microcr.stanford.edu>
2010-01-25 06:06:03 -0800

    Refactored create view and preliminarily hooked up backend
    I moved the article presentation from new.html.erb into a partial called _article.html.erb. I also added the
    javascript to post an Ajax request to the trails controller to return an article listing. I have not validated it
    against the actual site using net:http but I pan to do that in the next commit.

--- trailr/app/controllers/trails_controller.rb
+++ trailr/app/controllers/trails_controller.rb
@@ -1,3 +1,7 @@
+require 'json'
+
+
+
 class TrailsController < ApplicationController
 
   def show
@@ -22,6 +26,46 @@ class TrailsController < ApplicationController
   
   def slideshow
     render :layout => "slideshow"
+  end
+
+  def process_urls
+
+    url_regex = /(^$)|(^(http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+    
+    temp_urls = []
+    urls = params[:urls].split(/\s+/);
+    
+    for url in urls do
+      if(url.match(url_regex)) then
+        temp_urls << url
+      end        
+    end
+    
+    urls = temp_urls
+    puts urls;
+    
+    
+    
+    
+    
+    
+    data = []
+    
+    hash = {
+      :url => "http://www.google.com",
+      :header => "Some header information",
+      
+      :date => Time.now.rfc2822,
+      :source => "cnn.com",
+      
+      :notes => "\"This is a sample paragraph that is included\""
+    }
+    
+    
+    
+
+    render :partial => "article", :locals => hash
+    
   end
 
 end
