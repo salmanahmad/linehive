2cbae0859ee5e30792714cedc842db5a05acb905
Salman Ahmad <salmanahmad@microcr.stanford.edu>
2010-01-26 10:45:24 -0800

    Implemented simple image extraction - extraction algorithm still not perfect!
    I have completed the first attempt at extracting the images from a url and presenting a simple UI to allow
    users to switch between them. This currently relies of certian HTML properties to be set (namely height
    and width) which are not always present. In the future we need a better way of determining good "images"
    to use.

--- trailr/app/controllers/trails_controller.rb
+++ trailr/app/controllers/trails_controller.rb
@@ -49,43 +49,42 @@ class TrailsController < ApplicationController
     end
     
     urls = temp_urls
-    puts urls;
     
     @data = []
     
-    
     for url in urls do
       begin
-        
-        puts url
-        
-        open(url) do |f|
 
+        open(url) do |f|
 
           date_regex = Regexp.new("(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])");
           meta_regex = Regexp.new("<meta\s+name=\"description\"\s+content=\"(.*?)\".*?>", Regexp::IGNORECASE)
           p_regex = Regexp.new("<p[^>]*>(.*?)</p>", Regexp::IGNORECASE);
           title_regex = Regexp.new("<title[^>]*>(.*?)</title>", Regexp::IGNORECASE);
+          img_regex = Regexp.new("<img[^>]*/?>", Regexp::IGNORECASE);
 
           html = f.read
-          
-          
-          
+                    
+          uri = URI.parse(url)
           host = URI.parse(url).host
+          dirname = File.dirname(URI.parse(url).path)
 
           title = title_regex.match(html)
           date = title_regex.match(html)
           desc = meta_regex.match(html)
           
-
-
-
           if(title) then title = title[1] else title = nil end 
           if(date) then date = date[1] else date = nil end 
           
           if(desc) then
             desc = desc[1] 
             desc.gsub!(/<\/?[^>]*>/, "")
+            desc.chomp!
+            
+            if desc != ""
+              desc = "#{desc}"
+            end
+            
           else 
             desc = p_regex.match(html);
             if(desc) then
@@ -94,6 +93,43 @@ class TrailsController < ApplicationController
               desc = nil 
             end
           end
+          
+
+          #threshold = 2000
+          threshold = 10000
+          urls = []
+          html.scan(img_regex) do |match|
+            
+            width_regex = Regexp.new("width=\"(.*?)\"")
+            width_regex = Regexp.new(/(width)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/)
+            height_regex = Regexp.new("height=\"(.*?)\"")
+            height_regex = Regexp.new(/(width)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/)
+            src_regex = Regexp.new("src=\"(.*?)\"")
+
+            width = width_regex.match(match)      
+            height = height_regex.match(match)
+            src = src_regex.match(match)
+
+            if(width) then width = width[2].to_i else width = 0 end
+            if(height) then height = height[2].to_i else height = 0 end
+            if(src) then src = src[1] else src = nil end
+
+            if(src && height * width >= threshold) 
+
+              if(src.match(/^http:/))
+                src = src
+              elsif(src.match(/^\//))
+                src = "#{uri.scheme}://#{host}#{src}"
+              else
+                src = "#{uri.scheme}://#{host}#{dirname}/#{src}"
+              end
+              
+              urls << src
+
+
+            end
+          end
+          
           
           hash = {
             :url => url,
@@ -104,7 +140,8 @@ class TrailsController < ApplicationController
             
             :source => host,
 
-            :notes => "\"#{desc}\""
+            :notes => desc,
+            :urls => urls
           }
           
  
@@ -119,22 +156,7 @@ class TrailsController < ApplicationController
       end
     
     end
-    
-    
-    
-=begin    
-    hash = {
-      :url => "http://www.google.com",
-      :header => "Some header information",
-      
-      :date => Time.now.rfc2822,
-      :source => "cnn.com",
-      
-      :notes => "\"This is a sample paragraph that is included\""
-    }
-=end    
-    
-    
+        
     render :template => false
     
   end
