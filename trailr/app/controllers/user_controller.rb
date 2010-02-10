1917a159659a7310afbb24e5a549a120140d6686
U-Frank-Lenovo\Frank <Frank@Frank-Lenovo.(none)>
2010-02-10 11:29:07 -0800

    Title pages, some progress on user acconuts, front page, embed & urls in show.html

--- trailr/app/controllers/user_controller.rb
+++ trailr/app/controllers/user_controller.rb
@@ -1,2 +1,29 @@
+require 'md5'
+
 class UserController < ApplicationController
+  def signon
+    if current_user
+      redirect_to :controller => :home, :action => :index
+    end
+  end
+  
+  def process_signon
+    if request.post?
+       if user = User.authenticate(params[:user][:email], params[:user][:password])
+         session[:user] = user
+         redirect_to :controller => :home, :action => :index
+       else
+         session[:user] = nil
+         redirect_to :controller => "user", :action => "signon"            
+       end
+       
+     end
+    
+  end
+
+  def signout
+    session[:user] = nil
+    redirect_to :action => "signon"
+  end
+
 end
