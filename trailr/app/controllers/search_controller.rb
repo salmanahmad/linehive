23fca254e5f2e776b65d748ca36f542c52e61cd8
Salman Ahmad <salman@salmanahmad.com>
2010-02-15 01:15:39 -0800

    Initial implementation of the search functionality

--- trailr/app/controllers/search_controller.rb
+++ trailr/app/controllers/search_controller.rb
@@ -2,6 +2,25 @@ class SearchController < ApplicationController
 
   def results
     
+    @trails = []
+    
+    if(params[:query])
+      
+      
+      sql = <<-EOF 
+      SELECT DISTINCT trails.* FROM trails 
+        JOIN articles ON articles.trail_id = trails.id 
+        LEFT JOIN users ON trails.user_id = users.id
+      WHERE trails.caption LIKE ? OR users.handle LIKE ? OR articles.headline LIKE ? OR articles.source LIKE ? OR articles.url LIKE ?
+      ORDER BY viewcount DESC, created_at DESC;
+      EOF
+      
+      query = '%' + params[:query] + '%';
+      
+      @trails = Trail.find_by_sql([sql, query, query, query, query, query ]) if params[:query]
+      
+    end
+    
     respond_to do |format|
       format.html { render }
     end    
