aa66985b9055db5f6c9dc3acd3db8e57501be306
Salman Ahmad <salman@salmanahmad.com>
2010-03-04 00:47:25 -0800

    Added user validation

--- trailr/app/models/user.rb
+++ trailr/app/models/user.rb
@@ -4,6 +4,17 @@ class User < ActiveRecord::Base
   has_many :trails, :order => "created_at DESC"
   has_many :articles, :through => :trails
   
+  validates_presence_of :first_name 
+  validates_presence_of :last_name 
+
+  validates_presence_of :password, :message => "did not match, or password fields left blank", :on => :create
+
+  validates_uniqueness_of :email
+  validates_presence_of :email 
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "Provide valid email"
+  
+  
+  
   def published_trails
     Trail.find(:all, :conditions => {:user_id => self.id, :draft => false})
   end
